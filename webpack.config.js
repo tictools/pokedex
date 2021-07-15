6e67de68bb0524cb1c74db1d0ad33990554fa708
Jaume Villarreal <Jaume.Villarreal@webfleet.com>
2021-07-15 12:28:23 +0200

    Add webpack configuration and required dependencies

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const babelRules = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader'
+}
+
+const cssRules = {
+  test: /\.css$/,
+  use: [
+    "style-loader",
+    {
+      loader: "css-loader",
+      options: {
+        modules: {
+          localIdentName: "[name]__[local]--[hash:base64:5]",
+        },
+        importLoaders: 1,
+      },
+    },
+    "postcss-loader",
+  ],
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.[contenthash].js',
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [babelRules, cssRules]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Pokédex",
+      template: "./src/index.html",
+      hash: true
+    })
+  ]
+}
