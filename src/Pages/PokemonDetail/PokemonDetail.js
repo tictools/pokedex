9e17ffe6c12fe46cc4dc1de1919f71336d14949c
Jaume Villarreal <Jaume.Villarreal@webfleet.com>
2021-07-15 15:30:54 +0200

    Add first approach for List and Detail views.

---
+++ src/Pages/PokemonDetail/PokemonDetail.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function PokemonDetail () {
+  return (
+    <div>
+      <h2>Pokemon Detail</h2>
+    </div>
+  )
+}
---
+++ src/Pages/PokemonList/PokemonList.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function PokemonList () {
+  return (
+    <div>
+      <h2>Pokemon List</h2>
+    </div>
+  )
+}
