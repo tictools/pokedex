4b0fb9feb6bf87ca5a38024cf53a830cdc2fbff6
tictools <jaumevillarreal@gmail.com>
2021-07-17 11:06:52 +0200

    Added first approach Button component.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Button.css";
+
+export default function Button({ label, handleClick, status }) {
+  const buttonClassName = status
+    ? styles["button__pagination"]
+    : styles["button__pagination--disabled"];
+
+  return (
+    <button className={buttonClassName} onClick={handleClick}>
+      {label}
+    </button>
+  );
+}
