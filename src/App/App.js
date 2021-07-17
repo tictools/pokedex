63eea102c03033adc6d994185e37fae018572172
tictools <jaumevillarreal@gmail.com>
2021-07-17 10:58:15 +0200

    Update routes. Add state and handle state method

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,31 @@
-import * as React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import PokemonList from '../Pages/PokemonList/PokemonList'
-import PokemonDetail from '../Pages/PokemonDetail/PokemonDetail'
+import PokemonList from "../Pages/PokemonList/PokemonList";
+import { PokemonDetailWithRouter as PokemonDetail } from "../Pages/PokemonDetail/PokemonDetail";
+
+import { PAGINATION } from "../common/constants";
 
 export const App = () => {
+  const [page, setPage] = useState(PAGINATION.INITIAL_VALUE);
+
+  const handleChangePage = (value) => {
+    setPage(value);
+  };
+
   return (
     <div>
       <h1>Pokédex</h1>
       <Router>
         <Switch>
-          <Route exact path='/'>
-            <PokemonList />
+          <Route exact path="/">
+            <PokemonList page={page} handleChangePage={handleChangePage} />
           </Route>
-          <Route path='/detail'>
+          <Route path="/detail/:id">
             <PokemonDetail />
           </Route>
         </Switch>
       </Router>
     </div>
-  )
-}
+  );
+};
