83ef83852540f361623e79c8db045c731958c356
tictools <jaumevillarreal@gmail.com>
2021-07-17 11:00:40 +0200

    Add first approach for list pagination.

--- src/Pages/PokemonList/PokemonList.js
+++ src/Pages/PokemonList/PokemonList.js
@@ -1,9 +1,74 @@
-import * as React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function PokemonList () {
+import Button from "../../components/Button/Button";
+
+import {
+  BASE_URL,
+  EMPTY_ARRAY,
+  INITIAL_PAGINATION_STATUS_VALUE,
+} from "../../common/constants";
+import { getIdFromBaseUrl, getCurrentPage } from "../../common/utils";
+
+export default function PokemonList({ page, handleChangePage }) {
+  const [loading, setLoading] = useState(true);
+  const [pokemons, setPokemons] = useState(EMPTY_ARRAY);
+  const [pageStatus, setPageStatus] = useState(INITIAL_PAGINATION_STATUS_VALUE);
+
+  useEffect(() => {
+    const fetchPokemons = () => {
+      const currentPage = getCurrentPage(page);
+      setLoading(true);
+      fetch(`${BASE_URL}?offset=${currentPage}&limit=10`)
+        .then((response) => response.json())
+        .then(({ results, previous, next }) => {
+          setPokemons(results);
+          setPageStatus({
+            ...pageStatus,
+            previous,
+            next,
+          });
+          setLoading(false);
+        });
+    };
+    fetchPokemons();
+  }, [page]);
+
+  const content =
+    pokemons &&
+    pokemons.map((pokemon) => {
+      const id = getIdFromBaseUrl(pokemon.url, BASE_URL);
+      return (
+        <li key={id}>
+          <Link to={`/detail/${id}`}>{pokemon.name}</Link>
+        </li>
+      );
+    });
+
+  const handlePrevious = () => handleChangePage(page - 1);
+  const handleNext = () => handleChangePage(page + 1);
+
   return (
     <div>
       <h2>Pokemon List</h2>
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <ul>{content}</ul>
+          <Button
+            label="Previous"
+            handleClick={handlePrevious}
+            status={pageStatus.previous}
+          />
+          <p>{page + 1}</p>
+          <Button
+            label="Next"
+            handleClick={handleNext}
+            status={pageStatus.next}
+          />
+        </>
+      )}
     </div>
-  )
+  );
 }
