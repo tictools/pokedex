e7662f4bbae794a7dcf89043bb256b0a1acb6a80
tictools <jaumevillarreal@gmail.com>
2021-07-17 23:33:18 +0200

    Create custom hook for use pokemons

--- src/Pages/PokemonList/PokemonList.js
+++ src/Pages/PokemonList/PokemonList.js
@@ -5,67 +5,38 @@ import ButtonBar from "../../components/ButtonBar/ButtonBar";
 import FilterBar from "../../components/FilterBar/FilterBar";
 import ItemsList from "../../components/ItemsList/ItemsList";
 import Loader from "../../components/Loader/Loader";
+import usePokemons from "../../hooks/usePokemons";
 
-import {
-  BASE_URL,
-  EMPTY_ARRAY,
-  EMPTY_STRING,
-  INITIAL_PAGINATION_STATUS_VALUE,
-} from "../../common/constants";
-import { getIdFromBaseUrl, getCurrentOffset } from "../../common/utils";
+import { BASE_URL, EMPTY_STRING } from "../../common/constants";
+import { getIdFromBaseUrl } from "../../common/utils";
 
 export default function PokemonList({ page, handleChangePage }) {
-  const [loading, setLoading] = useState(true);
-  const [pokemons, setPokemons] = useState(EMPTY_ARRAY);
+  const { pokemons, loading, pageStatus } = usePokemons(page);
   const [filterPokemons, setFilterPokemons] = useState(EMPTY_STRING);
-  const [pageStatus, setPageStatus] = useState(INITIAL_PAGINATION_STATUS_VALUE);
-
-  useEffect(() => {
-    const fetchPokemons = () => {
-      const currentOffset = getCurrentOffset(page);
-      setLoading(true);
-      fetch(`${BASE_URL}?offset=${currentOffset}&limit=10`)
-        .then((response) => response.json())
-        .then(({ results, previous, next }) => {
-          setPokemons(results);
-          setPageStatus({
-            ...pageStatus,
-            previous,
-            next,
-          });
-          setLoading(false);
-        });
-    };
-    fetchPokemons();
-  }, [page]);
 
   const handlePrevious = () => handleChangePage(page - 1);
   const handleNext = () => handleChangePage(page + 1);
-
-  const getFilteredPokemons = (pokemons, filter) => {
-    return pokemons.filter((pokemon) => pokemon.name.includes(filter));
-  };
 
   const handleFilterChange = (filter) => {
     setFilterPokemons(filter);
   };
 
-  const filteredPokemons = getFilteredPokemons(pokemons, filterPokemons);
-
-  const getFormattedPokemons = (pokemons) => {
-    return (
-      pokemons &&
-      pokemons.map((pokemon) => {
-        const id = getIdFromBaseUrl(pokemon.url, BASE_URL);
-        return (
-          <li key={id}>
-            <Link to={`/detail/${id}`}>{pokemon.name}</Link>
-          </li>
-        );
-      })
-    );
+  const getFilteredPokemons = (pokemons, filter) => {
+    return pokemons.filter((pokemon) => pokemon.name.includes(filter));
   };
 
+  const getFormattedPokemons = (pokemons) =>
+    pokemons &&
+    pokemons.map((pokemon) => {
+      const id = getIdFromBaseUrl(pokemon.url, BASE_URL);
+      return (
+        <li key={id}>
+          <Link to={`/detail/${id}`}>{pokemon.name}</Link>
+        </li>
+      );
+    });
+
+  const filteredPokemons = getFilteredPokemons(pokemons, filterPokemons);
   const formattedPokemons = getFormattedPokemons(filteredPokemons);
 
   return (
